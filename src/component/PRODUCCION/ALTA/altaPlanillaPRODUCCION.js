a4214d5c64247e0fb900ac6a19fb0b0ead5877ab
javier sosa <javier_85@hotmail.com>
2020-02-07 14:02:32 -0300

    avance form altaPlanillaProduccion

--- src/component/PRODUCCION/ALTA/altaPlanillaPRODUCCION.js
+++ src/component/PRODUCCION/ALTA/altaPlanillaPRODUCCION.js
@@ -1,12 +1,44 @@
 import React from 'react'
-import {Form,Col,Row} from 'react-bootstrap'
+import {Form,Col,Row,Button} from 'react-bootstrap'
 import './styleAltaPlanillaPRODUCCION.css'
-import Operarios from './OPERARIOSPLANILLA/operariosPLANILLA'
 
 class AltaPlanillaPRODUCCION extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            fechaProduccion:undefined,
+            fechaFundicion:undefined,
+            idTurno:undefined,
+            HoraInicioProduccion:undefined,
+            HoraFinProduccion:undefined,
+            idOperacion:undefined,
+            idMaquina:undefined,
+            idPieza:undefined,
+            idMolde:undefined,
+            vecOperarios:[]
+        };
+    }
+    addOperario = e =>{
+        let Op = {
+            legajo:undefined,
+            nombre:undefined,
+            apellido:undefined,
+            horaInicio:undefined,
+            horaFin:undefined,
+            produccion:undefined,
+            calorias:undefined,
+            rechazos:[]
+        }
+        let newVecOperarios = [...this.state.vecOperarios,Op]
+        console.log(newVecOperarios[0])
+        this.setState({vecOperarios:newVecOperarios})
+    }
+    capturaDatos = e =>{
+        let nombre = e.target.name
+        let value =e.target.value
+        if(nombre === 'fechaProduccion'){this.setState({fechaProduccion:value})}
+        if(nombre === 'fechaFundicion'){this.setState({fechaFundicion:value})}
+        if(nombre === 'idTurno'){this.setState({idTurno:value})}
     }
     render() {
         return (
@@ -17,13 +49,13 @@ class AltaPlanillaPRODUCCION extends React.Component {
                         <Col>
                         <Form.Group controlId="fsdf" style={{width:'180px'}}>
                             <Form.Label>Fecha Produccion</Form.Label>
-                            <Form.Control type="date" required/>
+                            <Form.Control onChange={this.capturaDatos} name='fechaProduccion' type="date" required/>
                         </Form.Group>
                         </Col>
                         <Col>
                             <Form.Group controlId="formGsdfsdridEmail" style={{width:'180px'}}>
                                 <Form.Label>Fecha Fundicion</Form.Label>
-                                <Form.Control type="date" required />
+                                <Form.Control onChange={this.capturaDatos} name='fechaFundicion' type="date" required />
                             </Form.Group>
                         </Col>
                         <Col>
@@ -37,9 +69,9 @@ class AltaPlanillaPRODUCCION extends React.Component {
                         <Col>
                             <Form.Group  controlId="sdf" style={{width:'140px'}}>
                                 <Form.Label>Turno</Form.Label>
-                                <Form.Control as="select">
-                                    <option>1 - Mañana</option>
-                                    <option>2 - Tarde</option>
+                                <Form.Control onChange={this.capturaDatos} name='idTurno'  as="select">
+                                    <option value='1'>1 - Mañana</option>
+                                    <option value='2'>2 - Tarde</option>
                                 </Form.Control>
                             </Form.Group>
                         </Col>
@@ -105,8 +137,64 @@ class AltaPlanillaPRODUCCION extends React.Component {
                     <Row>
                         <Col>
                             <Form.Group>
-                                <Form.Label>Operarios</Form.Label>
-                                <Operarios/>
+                                        <Form.Label>Operarios</Form.Label>
+                                        <div style={{borderRadius:'7px',border:'#D5DBDB solid 1px',padding:'10px'}}>
+                            <Button variant="primary" size="lg" block onClick={this.addOperario}>
+                                Agregar
+                            </Button>
+                            {
+                                this.state.vecOperarios.map((o,i)=>{
+                                    return <div key={i}  style={{borderRadius:'7px',border:'#D5DBDB solid 1px',padding:'10px',marginTop:'10px'}}>
+                                                <Row>
+                                                    <Col>
+                                                        <Form.Group controlId="id_operaio">
+                                                            <Form.Label>Legajo</Form.Label>
+                                                            <Form.Control type='number'/>
+                                                        </Form.Group>
+                                                    </Col>
+                                                    <Col>
+                                                        <Form.Group controlId="nombre_operario">
+                                                            <Form.Label>Nombre</Form.Label>
+                                                            <Form.Control as="select">
+                                                                <option>Gracia Carlos</option>
+                                                                <option>Irusta</option>
+                                                            </Form.Control>
+                                                        </Form.Group>
+                                                    </Col>
+                                                    <Col>
+                                                            <Form.Group controlId="txt_desde" style={{width:'100px'}}>
+                                                                <Form.Label>Hora Inicio</Form.Label>
+                                                                <Form.Control type="time" style={{textAlign:'center'}}/>
+                                                            </Form.Group>
+                                                        </Col>
+                                                        <Col>
+                                                            <Form.Group controlId="txt_hasta" style={{width:'100px'}}>
+                                                                <Form.Label>Hora Fin</Form.Label>
+                                                                <Form.Control type="time" />
+                                                            </Form.Group>
+                                                        </Col>
+                                                        <Col>
+                                                            <Form.Group controlId="fsfdf" style={{width:'100px'}}>
+                                                                <Form.Label>Produccion</Form.Label>
+                                                                <Form.Control type="number" style={{textAlign:'center'}}/>
+                                                            </Form.Group>
+                                                        </Col>
+                                                        <Col>
+                                                            <Form.Group controlId="sdfs" style={{width:'100px'}}>
+                                                                <Form.Label>Calorias</Form.Label>
+                                                                <Form.Control type="number" />
+                                                            </Form.Group>
+                                                        </Col>
+                                                        <Col>
+                                                            <Button>
+                                                                Agregar rechazos
+                                                            </Button>
+                                                        </Col>
+                                                </Row>
+                                            </div>
+                                })
+                            }
+                    </div>
                             </Form.Group>
                         </Col>
                     </Row>
