c59f03ee589bc1386ce9bbd62fc29ceac4ca0b34
javier sosa <javier_85@hotmail.com>
2020-03-17 07:53:56 -0300

    add slider

--- src/component/LOGIN/indexLOGIN.js
+++ src/component/LOGIN/indexLOGIN.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import {withRouter} from 'react-router-dom'
 import FormLOGIN from './formLOGIN'
+import { Carousel } from 'react-bootstrap'
 import './styleLOGIN.css'
+import inyectora from '../../Imagenes/inyectora.jpg'
+import granallado from '../../Imagenes/granallado.jpg'
+import mecanizado from '../../Imagenes/mecanizado.jpg'
 
 class indexLOGIN extends React.Component {
     constructor(props){
@@ -12,9 +16,48 @@ class indexLOGIN extends React.Component {
     obtieneLargoWindows(){ return window.screen.height }
     render() {
         return (
-            <div id='ContenedorLogin'>
-                <div id='Slider'>
-                </div>
+            <div id='ContenedorLogin' style={{height:this.obtieneLargoWindows()+'px'}}>
+                <div id='Slider' style={{height:this.obtieneLargoWindows()+'px'}}>
+                    <Carousel >
+                        <Carousel.Item >
+                            <img
+                            style={{height:this.obtieneLargoWindows()+'px'}}
+                            className="d-block w-100"
+                            src= { granallado }
+                            alt="First slide"
+                            />
+                            <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            style={{height:this.obtieneLargoWindows()+'px'}}
+                            className="d-block w-100"
+                            src= { mecanizado }
+                            alt="Third slide"
+                            />
+                            <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            style={{height:this.obtieneLargoWindows()+'px'}}
+                            className="d-block w-100"
+                            src= { inyectora }
+                            alt="Third slide"
+                            />
+
+                            <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        </Carousel>
+                    </div>
                 <div className="FormLogin" style={{height:this.obtieneLargoWindows()+'px'}}>
                     <FormLOGIN/>
                 </div>
